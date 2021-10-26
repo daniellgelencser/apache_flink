6c4c118856dfb3e12f87ac2d44bcc8ca63f81513
daniellgelencser <daniel.l.gelencser@zohomail.com>
2021-10-26 19:40:10 +0200

    added virtual environment

--- install.sh
+++ install.sh
@@ -1,25 +1,31 @@
-mkdir dep
-cd dep
-
 echo "Get Python 3.8.12"
 wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
 tar -xzf Python-3.8.12.tgz
 rm Python-3.8.12.tgz
 
-echo "Install Python"
 cd Python-3.8.12
+
+echo "Install Python"
 ./configure --enable-optimizations
 make
-
-cd ../..
-ln -s dep/Python-3.8.12/python py38
 
 echo "Get pip"
 wget https://bootstrap.pypa.io/get-pip.py
 
 echo "Install pip"
-./py38 get-pip.py
+./python get-pip.py
 rm get-pip.py
 
+echo "Install virtualenv"
+./python -m pip install virtualenv==20.9.0
+
+cd ..
+
+echo "Create virtual environment"
+Python-3.8.12/python -m virtualenv venv
+
+echo "Activate virtual environment"
+source venv/bin/activate
+
 echo "Install Apache Flink 1.14.0"
-./py38 -m pip install apache-flink==1.14.0
+pip install apache-flink==1.14.0
