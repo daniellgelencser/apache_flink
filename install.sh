8b744cac321fa26901376026830519b0a8341b65
daniellgelencser <daniel.l.gelencser@zohomail.com>
2021-10-26 17:36:21 +0200

    added dependency list

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
+mkdir dep
 cd dep
 
-# echo "Get Python 3.8.12"
-# wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
-# tar -xzf Python-3.8.12.tgz
-# rm Python-3.8.12.tgz
+echo "Get Python 3.8.12"
+wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+tar -xzf Python-3.8.12.tgz
+rm Python-3.8.12.tgz
 
-# echo "Install Python"
+echo "Install Python"
 cd Python-3.8.12
-# ./configure --enable-optimizations
-# make
+./configure --enable-optimizations
+make
 
-# echo "Get pip"
-# wget https://bootstrap.pypa.io/get-pip.py
+echo "Get pip"
+wget https://bootstrap.pypa.io/get-pip.py
 
-# echo "Install pip"
-# ./python get-pip.py
-# rm get-pip.py
-
-cd ..
+echo "Install pip"
+./python get-pip.py
+rm get-pip.py
 
 echo "Install depencencies"
-Python-3.8.12/python -m pip install -r dependencies.txt
+Python-3.8.12/python -m pip install -r ../../dependencies.txt
