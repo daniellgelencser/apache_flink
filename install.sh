ca34f852c57c811e8997a5d1e8ba741a5d49d566
daniellgelencser <daniel.l.gelencser@zohomail.com>
2021-10-26 17:30:36 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 cd dep
 
-echo "Get Python 3.8.12"
-wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
-tar -xzf Python-3.8.12.tgz
-rm Python-3.8.12.tgz
+# echo "Get Python 3.8.12"
+# wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+# tar -xzf Python-3.8.12.tgz
+# rm Python-3.8.12.tgz
 
-echo "Install Python"
+# echo "Install Python"
 cd Python-3.8.12
-./configure --enable-optimizations
-make
+# ./configure --enable-optimizations
+# make
 
-echo "Get pip"
-wget https://bootstrap.pypa.io/get-pip.py
+# echo "Get pip"
+# wget https://bootstrap.pypa.io/get-pip.py
 
-echo "Install pip"
-./python get-pip.py
-rm get-pip.py
+# echo "Install pip"
+# ./python get-pip.py
+# rm get-pip.py
+
+cd ..
 
 echo "Install depencencies"
-./python -m pip install -r dependencies
+Python-3.8.12/python -m pip install -r dependencies.txt
