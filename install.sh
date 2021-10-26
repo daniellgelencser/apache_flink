78ca312e42bf389cbddad90ddaaf879d93c34950
daniellgelencser <daniel.l.gelencser@zohomail.com>
2021-10-26 17:30:21 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+cd dep
+
+echo "Get Python 3.8.12"
+wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+tar -xzf Python-3.8.12.tgz
+rm Python-3.8.12.tgz
+
+echo "Install Python"
+cd Python-3.8.12
+./configure --enable-optimizations
+make
+
+echo "Get pip"
+wget https://bootstrap.pypa.io/get-pip.py
+
+echo "Install pip"
+./python get-pip.py
+rm get-pip.py
+
+echo "Install depencencies"
+./python -m pip install -r dependencies
